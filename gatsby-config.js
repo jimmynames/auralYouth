36b10d0d263eff95eca930a8bfc8204b536d8a4f
Jimmy Names <jim.me.ryan@gmail.com>
2018-02-21 14:00:05 +0000

    removed plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,25 +11,25 @@ module.exports = {
       options: {
         fonts: [ `Lato` ]
       }
-    },
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: './src/favicon.png',
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: false,
-          yandex: false,
-          windows: false
-        }
-      }
     }
+    // {
+    //   resolve: `gatsby-plugin-favicon`,
+    //   options: {
+    //     logo: './src/favicon.png',
+    //     injectHTML: true,
+    //     icons: {
+    //       android: true,
+    //       appleIcon: true,
+    //       appleStartup: true,
+    //       coast: false,
+    //       favicons: true,
+    //       firefox: true,
+    //       twitter: false,
+    //       yandex: false,
+    //       windows: false
+    //     }
+    //   }
+    // }
     // {
     //   resolve: `gatsby-plugin-google-analytics`,
     //   options: {
