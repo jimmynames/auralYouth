8ca8db1a40742cec17c274386b4ee75a97bcd9db
Jimmy Names <jim.me.ryan@gmail.com>
2018-02-21 13:24:17 +0000

    plugfin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,41 @@
 module.exports = {
   siteMetadata: {
-    title: `Aural Youth`,
+    title: `Aural Youth`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-favicon`,
     {
-     resolve: `gatsby-plugin-google-fonts`,
-     options: {
-       fonts: [ `Lato` ]
-     }
-   }
-  //   {
-  //   resolve: `gatsby-plugin-google-analytics`,
-  //   options: {
-  //     trackingId: "UA-112975901-1",
-  //     anonymize: true,
-  //   },
-  // }
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [ `Lato` ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: 'UA-112975901-1',
+    //     anonymize: true
+    //   }
+    // }
   ]
 }
