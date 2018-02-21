865d06d157ba89186975a699a305b01fcd7242dd
Jimmy Names <jim.me.ryan@gmail.com>
2018-02-21 17:42:21 +0000

    added 100% width

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,21 +1,16 @@
-require('normalize.css')
-
-import React from "react"
-import PropTypes from "prop-types"
-import Link from "gatsby-link"
-import Helmet from "react-helmet"
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import withRouter from 'react-router-dom/withRouter'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import styled, { ThemeProvider, injectGlobal } from 'styled-components'
 // import { StickyContainer, Sticky } from 'react-sticky'
 
-import withRouter from 'react-router-dom/withRouter'
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
-
-import { Wrap } from './../components/Wrap'
-import BannerComp from './../components/Banner'
 import FooterComp from './../components/Footer'
-import Navigation from './../components/Navigation'
 
 import './animations.css'
+
+require('normalize.css')
 
 injectGlobal`
   body {
@@ -23,17 +18,12 @@ injectGlobal`
   }
 `
 
-
 const PageRender = styled.div`
   width: 100%;
   height: 100%;
-	background: white;
+background: white;
   @media (max-width: 693px) {}
   @media (max-width: 414px) {}
-`
-
-const Container = styled.div`
-  position: relative;
 `
 
 // const theme = {
@@ -43,81 +33,45 @@ const Container = styled.div`
 // }
 
 class TransitionHandler extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.props.location.pathname === window.location.pathname;
+  shouldComponentUpdate (nextProps, nextState) {
+    return this.props.location.pathname === window.location.pathname
   }
 
-  render() {
+  render () {
     const {children} = this.props;
     return (
-      <div className="transition-container">
+      <div className='transition-container'>
         {children}
       </div>
-    );
+    )
   }
 }
 
 const TemplateWrapper = ({ children, location }) => (
-  // static propTypes = {
-  //   children: PropTypes.func,
-  // }
-  //
-  // render() {
-	// 	return (
-
-			<div>
-    		<Helmet
-          title="Aural Youth"
-          meta={[
-            { name: "description", content: "Aural Youth" },
-            { name: "keywords", content: "Aural Youth" },
-          ]}
+		<div>
+    	<Helmet
+      title='Aural Youth'
+      meta={[
+            { name: 'description', content: 'Aural Youth' },
+            { name: 'keywords', content: 'Aural Youth' }
+      ]}
         />
-
-					{/* <BannerComp /> */}
-
-					{/* <StickyContainer>
-			<Sticky>
-				{
-					({
-						style,
-
-						// the following are also available but unused in this example
-						isSticky,
-						wasSticky,
-						distanceFromTop,
-						distanceFromBottom,
-						calculatedHeight
-					}) => {
-						return (
-							<header style={style}> */}
-								{/* <Navigation /> */}
-							{/* </header>
-						)
-					}
-				}
-			</Sticky> */}
-
-
-		        {/* <PageRender id="page" className='page-render'> */}
-						<TransitionGroup>
-      <CSSTransition
+				<TransitionGroup>
+      		<CSSTransition
           key={location.pathname}
           classNames="example"
           timeout={{ enter: 500, exit: 300 }}
       >
-        <TransitionHandler
+        		<TransitionHandler
             location={location}
-
         >
-					<PageRender>
-		          {children()}
-		        </PageRender>
-						<FooterComp />
-					</TransitionHandler>
-	 		</CSSTransition>
+							<PageRender>
+		          	{children()}
+		        	</PageRender>
+							<FooterComp />
+						</TransitionHandler>
+	 			</CSSTransition>
  		</TransitionGroup>
-
 				{/* </StickyContainer> */}
 				</div>
     )
