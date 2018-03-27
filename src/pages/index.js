2c57aecceb25719aee94656110676f8f6b1b6e6b
Jimmy Names <jim.me.ryan@gmail.com>
2018-03-27 19:11:52 +0100

    content changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,13 +24,13 @@ export default class Index extends React.Component {
             src={keys}
             src2x={keys2x}
             title='About'
-            para1='A forward-looking, sound production collective based in North London. We provide content production and technical services for the ever changing music industry, adapting our skill set to best suit the client’s needs. Go to the “Services” section of our website to see what we have to offer' />
+            para1="A forward-looking, sound production collective based in North London. We provide content production and technical services for the ever changing music industry, adapting our skill set to best suit the client's needs. Go to the 'Services' section of our website to see what we have to offer." />
           <ImageTextComp
             alt
             src={dudes}
             src2x={dudes2x}
             title='Bio'
-            para1='After both graduating with BA (Hons) in music performing in a variety of musical acts and working in different areas of the music industry; Benjamin Nash and Matt Noades founded Aural Youth with the ambition to record/produce up and coming bands.'
+            para1='After both graduating with BA (Hons) in music performing in a variety of musical acts and working in different areas of the music industry, Benjamin Nash and Matt Noades founded Aural Youth with the ambition to record and produce up and coming acts.'
             para2='With the desire to create a scene that helped exhibit the talent of the musical youth of today, the company eventually evolved into a fully fledged sound production company. Now a dedicated team of engineers and producers, we believe in creating a professional yet enjoyable work environment to maximise the best possible results for everyone involved.' />
         </Wrap>
         <FooterComp />
